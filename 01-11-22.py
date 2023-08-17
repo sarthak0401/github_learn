9a37783ed2fd977423239c7475d146fc9fa9183d
sarthak0401 <120019344+sarthak0401@users.noreply.github.com>
2023-08-17 14:34:09 +0530

    Add files via upload

---
+++ 01-11-22.py
@@ -0,0 +1,33 @@
+# #Sum of two given numbers
+# a = 2
+# b = 3.5
+# sum = a + b
+# print("the value of {0} + {1} is = {2}".format(a,b,sum))
+
+# #Sum of two numbers input from the user
+# c = input("Enter Number1\n")
+# d = input("Enter Number2\n")
+# sum2 = float(c)+ float(d)
+# print("the sum of {0} + {1} is {2}".format(c,d,sum2))
+
+
+# #Area of circle
+# e = float(input("Enter the radius\n"))
+# print("The area of circle is {0}".format(3.14*e**2))
+
+
+#Area of triangle
+# s1 = float(input('Enter the length of side1 '))
+# s2 = float(input('Enter the length of side2 '))
+# s3 = float(input('Enter the length of side3 '))
+# s = (s1+s2+s3)/2
+# area2 = (s*(s-s1)*(s-s2)*(s-s3))**(0.5)
+# print('The area of triangle is ','%0.2f' %(area2))
+
+
+# #Average of marks of three subjects
+# m1 = float(input("Enter the marks in subject1 "))
+# m2 = float(input("Enter the marks in subject2 "))
+# m3 = float(input("Enter the marks in subject3 "))
+# avg = (float)((m1+m2+m3)/3)
+# print('%0.2f'%avg)
